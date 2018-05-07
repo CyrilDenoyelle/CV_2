992c55f4acaa50bad3c187ed7ca518feb2c8c3e9
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2018-05-07 11:16:44 +0200

    comments on paralax js

--- resources/js/index-parallax.js
+++ resources/js/index-parallax.js
@@ -1,26 +1,17 @@
-var s = skrollr.init()
-
-
-
-var vid = $('.droneVideo');
-var scrollRatio = 0;
-var vidXPositionScreenRatio = vid.position().top-1000;
-// console.log(vid)
+// VIDEO JS
+var vid = $('.droneVideo'),
+scrollRatio = 0,
+vidXPositionScreenRatio;
 $(window).scroll(function(){
-	// console.log($(window).scrollTop())
-	var vidXPositionScreenRatio = vid.position().top*0.8;
-	scrollRatio = ($(window).scrollTop()-(vidXPositionScreenRatio+$(window).width()/10))/75;
+	vidXPositionScreenRatio = vid.position().top*0.8+$(window).width()/10;// (ratio entre la position de la video sur la page et la largeur de l'ecran, pour permettre le responsive)
+	scrollRatio = ($(window).scrollTop()-vidXPositionScreenRatio)/75;// rapport position de la video sur l'ecran / currentTime de la video
 });
-// console.log(3270-335+75)
 setInterval(function(){
-	// if(vid[0].currentTime < scrollRatio-0.25){
-	console.log()
-	vid[0].currentTime-=(vid[0].currentTime-scrollRatio)/20;
-
-	// }else if(vid[0].currentTime > scrollRatio){
-	// }else if(vid[0].currentTime > scrollRatio){
-	// 	vid[0].currentTime-=0.05;
-	// }else{
-	// 	vid[0].pause();
-	// }
+	//division par 20 de la difference entre le currentTime de la video et le scrollRatio
+	vid[0].currentTime-=(vid[0].currentTime-scrollRatio)/20;//valeur negative pour permettre la lecture a l'enver de la video quand on scroll vers le haut
 }, 100);
+
+// VIDEO JS END
+
+var s = skrollr.init();
+
