f5e3ada9160294c05032c7b453ecc96ee3e8086d
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2018-05-07 16:36:27 +0200

    more smoothness

--- resources/js/index-parallax.js
+++ resources/js/index-parallax.js
@@ -9,7 +9,7 @@ $(window).scroll(function(){
 });
 setInterval(function(){
 	//division par 20 de la difference entre le currentTime de la video et le scrollRatio, avec aplication uniquement si la difreente est sufisante pour ne pas coser de passage d'une frame a la foi.
-	vid[0].currentTime-=((vid[0].currentTime-scrollRatio)/10)>=0.05||((vid[0].currentTime-scrollRatio)/10)<=-0.05?(vid[0].currentTime-scrollRatio)/10:0;//valeur negative pour permettre la lecture a l'enver de la video quand on scroll vers le haut
+	vid[0].currentTime-=((vid[0].currentTime-scrollRatio)/10)>=0.05||((vid[0].currentTime-scrollRatio)/10)<=-0.05?(vid[0].currentTime-scrollRatio)/20:0;//valeur negative pour permettre la lecture a l'enver de la video quand on scroll vers le haut
 }, 100);
 
 // VIDEO JS END
