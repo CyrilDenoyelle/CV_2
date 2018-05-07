4a2d80edc617bf7d8bb58157fa3917c684087b40
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2018-05-07 16:48:23 +0200

    no consollog

--- resources/js/index-parallax.js
+++ resources/js/index-parallax.js
@@ -4,7 +4,6 @@ scrollRatio = 0,
 vidXPositionScreenRatio;
 $(window).scroll(function(){
 	vidXPositionScreenRatio = vid.position().top*0.8;// (ratio entre la position de la video sur la page et la largeur de l'ecran, pour permettre le responsive)
-	console.log($(window).scrollTop());
 	scrollRatio = ($(window).scrollTop()-vidXPositionScreenRatio)/75;// rapport position de la video sur l'ecran / currentTime de la video
 });
 setInterval(function(){
